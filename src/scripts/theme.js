423fe823c30cfc344852e6376000a6f97201f490
unknown <aas.amorim01@gmail.com>
2023-06-20 19:50:58 -0300

    1º Entrega

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,19 @@
 /* Desenvolva sua lógica aqui ... */
+function renderDarkMode (){
+    const darkModeButton = document.querySelector(".darkMode__button")
+    const imageButton = document.querySelector(".img__button")
+    const body = document.body
+    
+    darkModeButton.addEventListener("click", ()=>{
+    body.classList.toggle("dark__mode")
+
+    if(body.classList.contains("dark__mode")){
+        imageButton.src = "./src/assets/img/sun.svg"
+        localStorage.setItem('darkMode', true)
+    }else{
+        imageButton.src = "./src/assets/img/moon.svg"
+        localStorage.setItem("darkMode", false)
+        }
+    })
+}
+renderDarkMode()
