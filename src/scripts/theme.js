735603c00fd195da367b79a88c61fa419d2d5d8f
unknown <aas.amorim01@gmail.com>
2023-06-21 11:39:09 -0300

    Ajustes no salvamento do LocalStorage

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -3,17 +3,28 @@ function renderDarkMode (){
     const darkModeButton = document.querySelector(".darkMode__button")
     const imageButton = document.querySelector(".img__button")
     const body = document.body
+
+    const isDarkMode = localStorage.getItem("darkMode")
     
+    if(isDarkMode === "true"){
+        body.classList.add("dark__mode")
+        imageButton.src = "./src/assets/img/sun.svg"
+    }else{
+        body.classList.remove("dark__mode")
+        imageButton.src = "./src/assets/img/moon.svg"
+    }
+
     darkModeButton.addEventListener("click", ()=>{
     body.classList.toggle("dark__mode")
 
     if(body.classList.contains("dark__mode")){
         imageButton.src = "./src/assets/img/sun.svg"
-        localStorage.setItem('darkMode', true)
+        localStorage.setItem("darkMode", "true")
+        
     }else{
         imageButton.src = "./src/assets/img/moon.svg"
-        localStorage.setItem("darkMode", false)
-        }
-    })
+        localStorage.setItem("darkMode", "false")
+    }
+})
 }
 renderDarkMode()
