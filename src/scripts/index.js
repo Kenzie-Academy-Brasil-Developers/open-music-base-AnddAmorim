a89777624dc6f6c51039f218fb13f18107c448b6
unknown <aas.amorim01@gmail.com>
2024-03-08 12:40:12 -0300

    ajustes e adições

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,96 +1,102 @@
-/* Desenvolva sua lógica aqui ... */
-
 function createCard(product) {
-   const card = document.createElement("li");
-   const albumImg = document.createElement("img");
-   const bandAndAgeName = document.createElement("p");
-   const albumTitle = document.createElement("h2");
+  const card = document.createElement("li");
+  const albumImg = document.createElement("img");
+  const bandAndAgeName = document.createElement("p");
+  const albumTitle = document.createElement("h2");
 
-   const span = document.createElement("span");
-   const priceSpan = document.createElement("p");
-   const buttonSpan = document.createElement("button");
+  const span = document.createElement("span");
+  const priceSpan = document.createElement("p");
+  const buttonSpan = document.createElement("button");
 
-   albumImg.src = product.img;
-   bandAndAgeName.innerText = `${product.band} ${product.year}`;
-   albumTitle.innerText = product.title;
+  albumImg.src = product.img;
+  bandAndAgeName.innerText = `${product.band} ${product.year}`;
+  albumTitle.innerText = product.title;
 
-   priceSpan.innerText = `R$ ${product.price}`;
-   buttonSpan.innerText = "Comprar";
-   
-   card.append(albumImg, bandAndAgeName, albumTitle, span);
-   span.append(priceSpan, buttonSpan);
+  priceSpan.innerText = `R$ ${product.price}`;
+  buttonSpan.innerText = "Comprar";
 
-   return card;
+  card.append(albumImg, bandAndAgeName, albumTitle, span);
+  span.append(priceSpan, buttonSpan);
+
+  return card;
 }
 
+function renderButtons(buttons) {
+  const genderContainer = document.querySelector(".gender__container");
 
-function renderButtons(buttons){
-   const genderContainer = document.querySelector(".gender__container")
+  buttons.forEach((category) => {
+    const categoryList = document.createElement("li");
+    const categoryButton = document.createElement("button");
 
-   buttons.forEach(category => {
-      const categoryList = document.createElement("li")
-      const categoryButton = document.createElement("button")
+    categoryButton.innerText = category;
+    categoryButton.classList = "buttons__categories";
 
-      categoryButton.innerText = category
-      categoryButton.classList = "buttons__categories"
-
-      categoryList.appendChild(categoryButton)
-      genderContainer.appendChild(categoryList)
-   });
+    categoryList.appendChild(categoryButton);
+    genderContainer.appendChild(categoryList);
+  });
 }
-renderButtons(categories)
+renderButtons(categories);
 
-function renderCards(cards){
-   const ulContainer = document.querySelector(".found__albuns")
+function renderCards(cards) {
+  const ulContainer = document.querySelector(".found__albuns");
 
-   cards.forEach(products =>{
-      const card = createCard(products)
-      ulContainer.appendChild(card)
-   })
+  cards.forEach((products) => {
+    const card = createCard(products);
+    ulContainer.appendChild(card);
+  });
 }
-renderCards(products)
+renderCards(products);
 
-function addEventsFilter(categories, products){
-   const ulContainer = document.querySelector(".found__albuns")
-   const filterButtons = document.querySelectorAll(".buttons__categories")
-   const priceInput = document.querySelector("input[type='range']")
-   const paragInput = document.querySelector(".input__father")
+function addEventsFilter(categories, products) {
+  const ulContainer = document.querySelector(".found__albuns");
+  const filterButtons = document.querySelectorAll(".buttons__categories");
+  const priceInput = document.querySelector("input[type='range']");
+  const paragInput = document.querySelector(".input__father");
 
-   let filteredArray = products
-   let categoryIndex = 0
-   let inputValue = 100
-   
-   filterButtons.forEach((button, index)=>{
-      button.innerText = categories[index]
+  let filteredArray = products;
+  let categoryIndex = 0;
+  let inputValue = 100;
 
-      button.addEventListener("click", ()=>{
-         categoryIndex = index
-         
-         if(categoryIndex === 0){
-            filteredArray= products.filter((product)=> product.price <= inputValue)
-         }else{
-            filteredArray = products.filter((product) => product.category === categoryIndex && product.price <=inputValue)
-         }
-         ulContainer.innerHTML = ""
-         
-         renderCards(filteredArray)
-         
-      })
-   })
+  priceInput.value = 100;
+  paragInput.innerText = `Até... R$: ${inputValue}`;
 
-   priceInput.addEventListener("input", ()=>{
-      inputValue = priceInput.value
-      paragInput.innerText = `Até... R$: ${inputValue}`
-      
-      if(categoryIndex === 0){
-         filteredArray = products.filter((product)=> product.price <= inputValue)
-      }else{
-         filteredArray = products.filter((product) => product.category === categoryIndex && product.price <= inputValue)
+  filterButtons.forEach((button, index) => {
+    button.innerText = categories[index];
+
+    button.addEventListener("click", () => {
+      categoryIndex = index;
+
+      if (categoryIndex === 0) {
+        filteredArray = products.filter(
+          (product) => product.price <= inputValue
+        );
+      } else {
+        filteredArray = products.filter(
+          (product) =>
+            product.category === categoryIndex && product.price <= inputValue
+        );
       }
-      ulContainer.innerHTML = ""
-      renderCards(filteredArray)
-      })
-   }
-   
+      ulContainer.innerHTML = "";
 
-addEventsFilter(categories, products)
+      renderCards(filteredArray);
+    });
+  });
+
+  priceInput.addEventListener("input", () => {
+    inputValue = priceInput.value;
+    paragInput.innerText = `Até... R$: ${inputValue}`;
+
+    if (categoryIndex === 0) {
+      filteredArray = products.filter((product) => product.price <= inputValue);
+    } else {
+      filteredArray = products.filter(
+        (product) =>
+          product.category === categoryIndex && product.price <= inputValue
+      );
+    }
+    ulContainer.innerHTML = "";
+    renderCards(filteredArray);
+  });
+}
+
+addEventsFilter(categories, products);
--- src/scripts/productsData.js
+++ src/scripts/productsData.js
@@ -35,19 +35,91 @@ const products = [
     year: 1993,
     id: 3,
   },
+  {
+    title: "Deixa Entrar",
+    category: 3,
+    price: 30.0,
+    img: "./src/assets/img/5.jpg",
+    band: "Falamansa",
+    year: 2000,
+    id: 4,
+  },
+  {
+    title: "Deixa a Vida Me Levar",
+    category: 4,
+    price: 60.0,
+    img: "./src/assets/img/6.jpg",
+    band: "Zeca Pagodinho",
+    year: 2002,
+    id: 5,
+  },
+  {
+    title: "Xamego",
+    category: 5,
+    price: 90.0,
+    img: "./src/assets/img/7.jpg",
+    band: "Luiz Gonzaga",
+    year: 1958,
+    id: 6,
+  },
+  {
+    title: "Racionais MC's",
+    category: 6,
+    price: 50.0,
+    img: "./src/assets/img/8.png",
+    band: "Racionais MC's",
+    year: 1994,
+    id: 7,
+  },
+  {
+    title: "Infinite",
+    category: 7,
+    price: 70.0,
+    img: "./src/assets/img/9.jpg",
+    band: "Eminem",
+    year: 1996,
+    id: 8,
+  },
+  {
+    title: "Live!",
+    category: 9,
+    price: 99.0,
+    img: "./src/assets/img/10.jpg",
+    band: "Bob Marley & The Wailers",
+    year: 1975,
+    id: 9,
+  },
+  {
+    title: "Unchained",
+    category: 10,
+    price: 45.0,
+    img: "./src/assets/img/11.jpg",
+    band: "Johnny Cash",
+    year: 1996,
+    id: 10,
+  },
+  {
+    title: "The Nu Nation Project",
+    category: 11,
+    price: 80.0,
+    img: "./src/assets/img/12.jpg",
+    band: "Kirk Franklin",
+    year: 1998,
+    id: 11,
+  },
 ];
 
 const categories = [
-  "Todos",
-  "Pop",
-  "MPB",
-  "Forró",
-  "Samba",
-  "Baião",
-  "Rap",
-  "Hip-Hop",
-  "Rock",
-  "Reggae",
-  "Country",
-  "Gospel",
+  "Todos", 
+  "Pop", /*categoria 1*/
+  "MPB",  /*categoria 2*/
+  "Forró", /*categoria 3*/
+  "Samba", /*categoria 4*/
+  "Baião",  /*categoria 5*/
+  "Rap", /*categoria 6*/
+  "Hip-Hop", /*categoria 7*/
+  "Rock", /*categoria 8*/
+  "Reggae", /*categoria 9*/
+  "Country", /*categoria 10*/
+  "Gospel", /*categoria 11*/
 ];
